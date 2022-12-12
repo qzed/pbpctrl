c9b52058567d62fc424644e897e3e5b9f392be68
Maximilian Luz <luzmaximilian@gmail.com>
2022-12-12 01:57:59 +0100

    libgfps: Fix formatting

--- libgfps/src/lib.rs
+++ libgfps/src/lib.rs
@@ -1,4 +1,4 @@
-//! Library for the Google Fast Pair Service protocol (GFPS).  Focussed on
+//! Library for the Google Fast Pair Service protocol (GFPS). Focussed on
 //! communication via the dedicated GFPS RFCOMM channel.
 //!
 //! See <https://developers.google.com/nearby/fast-pair> for the specification.
