7ff788660997f155fa32ae396f4546541b2cdab9
Maximilian Luz <luzmaximilian@gmail.com>
2022-12-21 20:32:19 +0100

    libmaestro: Add enum for RPC type

--- libmaestro/src/pwrpc/types.rs
+++ libmaestro/src/pwrpc/types.rs
@@ -27,6 +27,31 @@ pub enum RpcStatus {
 }
 
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum RpcType {
+    Unary,
+    ServerStream,
+    ClientStream,
+    BidirectionalStream,
+}
+
+impl RpcType {
+    pub fn has_server_stream(&self) -> bool {
+        match *self {
+            RpcType::ServerStream | RpcType::BidirectionalStream => true,
+            RpcType::Unary | RpcType::ClientStream => false,
+        }
+    }
+
+    pub fn has_client_stream(&self) -> bool {
+        match *self {
+            RpcType::ClientStream | RpcType::BidirectionalStream => true,
+            RpcType::Unary | RpcType::ServerStream => false,
+        }
+    }
+}
+
+
 mod generated {
     include!(concat!(env!("OUT_DIR"), "/pw.rpc.packet.rs"));
 }
