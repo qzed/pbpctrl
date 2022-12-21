a97fd4b04d8792a08168a4ff7b81c40957fe3842
Maximilian Luz <luzmaximilian@gmail.com>
2022-12-21 20:49:12 +0100

    libmaestro: examples/listen: Update example for new client implementation

--- libmaestro/examples/maestro_listen.rs
+++ libmaestro/examples/maestro_listen.rs
@@ -3,27 +3,25 @@
 //! Usage:
 //!   cargo run --example maestro_listen -- <bluetooth-device-address>
 
-use std::collections::HashMap;
 use std::str::FromStr;
 
 use bluer::{Address, Session, Device};
 use bluer::rfcomm::{Profile, ReqError, Role, ProfileHandle};
 
-use futures::StreamExt;
+use futures::{StreamExt, Sink};
 
-use maestro::pwrpc::id::{self, Identifier};
+use maestro::pwrpc::client::Client;
+use maestro::pwrpc::id::Identifier;
 use maestro::pwrpc::codec::{Codec, Packet};
-use maestro::pwrpc::types::PacketType;
-use maestro::protocol;
+use maestro::pwrpc::types::{RpcType, PacketType, RpcPacket};
+use maestro::protocol::{self, addr};
+
 use prost::Message;
 
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> bluer::Result<()> {
     env_logger::init();
-
-    // packet printer
-    let printer = Printer::create();
 
     // handle command line arguments
     let addr = std::env::args().nth(1).expect("need device address as argument");
@@ -58,56 +56,122 @@ async fn main() -> bluer::Result<()> {
     println!();
 
     // try to reconnect if connection is reset
-    loop {
-        let stream = {
-            // register GFPS profile
-            println!("Registering Maestro profile...");
+    let stream = {
+        // register GFPS profile
+        println!("Registering Maestro profile...");
 
-            let profile = Profile {
-                uuid: maestro::UUID,
-                role: Some(Role::Client),
-                require_authentication: Some(false),
-                require_authorization: Some(false),
-                auto_connect: Some(false),
-                ..Default::default()
-            };
-
-            let mut profile_handle = session.register_profile(profile).await?;
-
-            // connect profile
-            println!("Connecting GFPS profile...");
-            connect_device_to_profile(&mut profile_handle, &dev).await?
+        let profile = Profile {
+            uuid: maestro::UUID,
+            role: Some(Role::Client),
+            require_authentication: Some(false),
+            require_authorization: Some(false),
+            auto_connect: Some(false),
+            ..Default::default()
         };
 
-        println!("Profile connected");
+        let mut profile_handle = session.register_profile(profile).await?;
 
-        // listen to event messages
-        let codec = Codec::new();
-        let mut stream = codec.wrap(stream);
+        // connect profile
+        println!("Connecting GFPS profile...");
+        connect_device_to_profile(&mut profile_handle, &dev).await?
+    };
 
-        println!("Listening...");
-        println!();
+    println!("Profile connected");
 
-        while let Some(packet) = stream.next().await {
-            match packet {
-                Ok(packet) => {
-                    printer.handle(&packet);
-                    println!()
-                }
-                Err(e) if e.raw_os_error() == Some(104) => {
-                    // The Pixel Buds Pro can hand off processing between each
-                    // other. On a switch, the connection is reset. Wait a bit
-                    // and then try to reconnect.
-                    println!();
-                    println!("Connection reset. Attempting to reconnect...");
-                    tokio::time::sleep(std::time::Duration::from_millis(500)).await;
-                    break;
-                }
-                Err(e) => {
-                    Err(e)?
-                }
+    // listen to event messages
+    let codec = Codec::new();
+    let mut stream = codec.wrap(stream);
+
+    println!("Listening...");
+    println!();
+
+    let mut packet_addr = addr::Address::from_value(0);
+
+    while let Some(packet) = stream.next().await {
+        match packet {
+            Ok(packet) => {
+                packet_addr = addr::Address::from_value(packet.address).swap();
+                break;
+
+            }
+            Err(e) if e.raw_os_error() == Some(104) => {
+                // The Pixel Buds Pro can hand off processing between each
+                // other. On a switch, the connection is reset. Wait a bit
+                // and then try to reconnect.
+                println!();
+                println!("Connection reset. Attempting to reconnect...");
+                tokio::time::sleep(std::time::Duration::from_millis(500)).await;
+                break;
+            }
+            Err(e) => {
+                Err(e)?
             }
         }
+    }
+
+    let client = Client::new(stream);
+    let handle = client.handle();
+
+    tokio::spawn(run_client(client));
+
+    let mut call = handle.call(RpcType::Unary, Packet {
+        address: packet_addr.value(),
+        rpc: RpcPacket {
+            r#type: PacketType::Request as _,
+            channel_id: packet_addr.channel_id().unwrap(),
+            service_id: Identifier::new("maestro_pw.Maestro").hash(),
+            method_id: Identifier::new("GetSoftwareInfo").hash(),
+            payload: maestro::protocol::types::Empty{}.encode_to_vec(),
+            status: 0,
+            call_id: 42,
+        },
+    }).await?;
+
+    let response = call.result().await.unwrap();
+    {
+        let info = protocol::types::SoftwareInfo::decode(&response[..])
+            .expect("failed to decode SoftwareInfo packet");
+
+        println!("{:#?}", info);
+    }
+
+    let mut call = handle.call(RpcType::ServerStream, Packet {
+        address: packet_addr.value(),
+        rpc: RpcPacket {
+            r#type: PacketType::Request as _,
+            channel_id: packet_addr.channel_id().unwrap(),
+            service_id: Identifier::new("maestro_pw.Maestro").hash(),
+            method_id: Identifier::new("SubscribeToSettingsChanges").hash(),
+            payload: maestro::protocol::types::Empty{}.encode_to_vec(),
+            status: 0,
+            call_id: 42,
+        },
+    }).await?;
+
+    let mut changes = call.stream();
+    while let Some(data) = changes.next().await {
+        let data = data.unwrap();
+
+        let info = protocol::types::SettingsRsp::decode(&data[..])
+            .expect("failed to decode SettingsRsp packet");
+
+        println!("{:#?}", info);
+    }
+
+    Ok(())
+}
+
+async fn run_client<S, E>(mut client: Client<S>)
+where
+    S: Sink<Packet>,
+    S: futures::Stream<Item = Result<Packet, E>> + Unpin,
+    S::Error: std::fmt::Debug,
+    E: std::fmt::Debug,
+{
+    let result = client.run().await;
+
+    if let Err(e) = result {
+        log::error!("client shut down with error: {e:?}")
     }
 }
 
@@ -137,73 +201,5 @@ async fn connect_device_to_profile(profile: &mut ProfileHandle, dev: &Device)
                 }
             },
         }
-    }
-}
-
-
-struct Printer {
-    map: HashMap<(id::Hash, id::Hash), &'static dyn Fn(&Packet)>,
-}
-
-impl Printer {
-    fn create() -> Self {
-        let mut map = HashMap::<_, &'static dyn Fn(&Packet)>::new();
-
-        let id_maestro = Identifier::new("maestro_pw.Maestro");
-        let id_swinfo = Identifier::new("GetSoftwareInfo");
-
-        map.insert((id_maestro.hash(), id_swinfo.hash()), &print_packet_swinfo);
-
-        Self { map }
-    }
-
-    fn handle(&self, packet: &Packet) {
-        match self.map.get(&(packet.rpc.service_id, packet.rpc.method_id)) {
-            Some(handler) => handler(packet),
-            None => print_packet_default(packet)
-        }
-    }
-}
-
-fn print_packet_swinfo(packet: &Packet) {
-    println!("Frame:");
-    println!("  address: 0x{:04x}", packet.address);
-    println!("  packet:");
-    println!("    type:    {:?}", PacketType::from_i32(packet.rpc.r#type).unwrap());
-    println!("    channel: {:?}", packet.rpc.channel_id);
-    println!("    service: maestro_pw.Maestro");
-    println!("    method:  GetSoftwareInfo");
-    println!("    status:  {:?}", packet.rpc.status);
-    println!("    call-id: {:?}", packet.rpc.call_id);
-    println!("    payload:");
-
-    let info = protocol::types::SoftwareInfo::decode(&packet.rpc.payload[..])
-        .expect("failed to decode SoftwareInfo packet");
-
-    println!("{:#02x?}", info);
-}
-
-fn print_packet_default(packet: &Packet) {
-    println!("Frame:");
-    println!("  address: 0x{:04x}", packet.address);
-    println!("  packet:");
-    println!("    type:    {:?}", PacketType::from_i32(packet.rpc.r#type).unwrap());
-    println!("    channel: {:?}", packet.rpc.channel_id);
-    println!("    service: {:08x?}", packet.rpc.service_id);
-    println!("    method:  {:08x?}", packet.rpc.method_id);
-    println!("    status:  {:?}", packet.rpc.status);
-    println!("    call-id: {:?}", packet.rpc.call_id);
-    println!("    payload:");
-
-    let data = pretty_hex::config_hex(
-        &packet.rpc.payload,
-        pretty_hex::HexConfig {
-            title: false,
-            ..Default::default()
-        },
-    );
-
-    for line in data.lines() {
-        println!("      {}", line);
     }
 }
